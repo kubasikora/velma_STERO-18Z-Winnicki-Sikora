a37feac031c0ff6d7b079d77f48e33cfa4e7b123
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2018-11-11 16:07:47 +0100

    #5 script stub and reading can position

---
+++ scripts/die_Cola_ubertragen.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python2
+
+
+
+import roslib; roslib.load_manifest('velma_task_cs_ros_interface')
+import rospy
+
+from velma_common import *
+from rcprg_planner import *
+from rcprg_ros_utils import exitError
+
+ # define a function for frequently used routine in this test
+def planAndExecute(q_dest):
+    print "Planning motion to the goal position using set of all joints..."
+    print "Moving to valid position, using planned trajectory."
+    goal_constraint = qMapToConstraints(q_dest, 0.01, group=velma.getJointGroup("impedance_joints"))
+    for i in range(5):
+        rospy.sleep(0.5)
+        js = velma.getLastJointState()
+        print "Planning (try", i, ")..."
+        traj = p.plan(js[1], [goal_constraint], "impedance_joints", max_velocity_scaling_factor=0.15, planner_id="RRTConnect")
+        if traj == None:
+            continue
+        print "Executing trajectory..."
+        if not velma.moveJointTraj(traj, start_time=0.5):
+            exitError(5)
+        if velma.waitForJoint() == 0:
+            break      
+        else:
+            print "The trajectory could not be completed, retrying..."
+            continue
+    rospy.sleep(0.5)
+    js = velma.getLastJointState()
+    if not isConfigurationClose(q_dest, js[1]):
+        exitError(6)  
+
+if __name__ == "__main__":
+    # define some configurations
+    q_map_starting = {'torso_0_joint':0,
+        'right_arm_0_joint':-0.3,   'left_arm_0_joint':0.3,
+        'right_arm_1_joint':-1.8,   'left_arm_1_joint':1.8,
+        'right_arm_2_joint':1.25,   'left_arm_2_joint':-1.25,
+        'right_arm_3_joint':0.85,   'left_arm_3_joint':-0.85,
+        'right_arm_4_joint':0,      'left_arm_4_joint':0,
+        'right_arm_5_joint':-0.5,   'left_arm_5_joint':0.5,
+        'right_arm_6_joint':0,      'left_arm_6_joint':0 }
+
+    rospy.init_node('proj1_executor')
+    rospy.sleep(0.5)
+
+    print "Running python interface for Velma..."
+    velma = VelmaInterface()
+    print "Waiting for VelmaInterface initialization..."
+    if not velma.waitForInit(timeout_s=10.0):
+        print "Could not initialize VelmaInterface\n"
+        exitError(1)
+    print "Initialization ok!\n"
+    
+    if velma.enableMotors() != 0:
+        exitError(14)
+
+    diag = velma.getCoreCsDiag()
+    if not diag.motorsReady():
+        print "Motors must be homed and ready to use for this test."
+        exitError(1)
+
+    print "waiting for Planner init..."
+    p = Planner(velma.maxJointTrajLen())
+    if not p.waitForInit():
+        print "could not initialize PLanner"
+        exitError(2)
+    print "Planner init ok \n"
+    print "Waiting for octomap...\n"
+    oml = OctomapListener("/octomap_binary")
+    rospy.sleep(1.0)
+    octomap = oml.getOctomap(timeout_s=5.0)
+    p.processWorld(octomap)
+    print "octomap ready\n"
+
+    velma = VelmaInterface()
+    velma.waitForInit()
+    T_B_Jar = velma.getTf("B", "target")
+    print T_B_Jar
+
+
+
+    
+
+    
+
+
+    
